f9082be354f9d8c69962532efd8eb9e9d8913f29
Jonas Bak Phillipson <jbakchr@gmail.com>
2026-07-26 10:44:42 +0200

    feat(curl): implement interactive curl command builder

--- src/lx/tools/curl/build.py
+++ src/lx/tools/curl/build.py
@@ -1,2 +1,61 @@
-def build():
-    print("build curl command")
+import subprocess
+
+from rich import print
+from rich.prompt import Confirm
+from rich.prompt import Prompt
+
+from lx.ui.console import (
+    console,
+    header,
+    separator,
+)
+
+
+def build() -> None:
+    separator()
+    header("[bold]Build a real curl command step by step.[/bold]")
+    separator()
+
+    console.print()
+    url = Prompt.ask(
+        "Which URL do you want to request?",
+        default="https://api.github.com",
+    )
+
+    console.print()
+    separator()
+
+    command = f'curl "{url}"'
+
+    print()
+    print("[bold cyan]Generated curl command[/bold cyan]")
+    print()
+    print(f"👉 {command}")
+
+    print()
+    print("[bold]How To Read This Command[/bold]")
+    print()
+
+    print("curl")
+    print("→ make an HTTP request")
+    print()
+
+    print(url)
+    print("→ the address we want to contact")
+
+    print()
+
+    separator()
+
+    print()
+
+    if Confirm.ask("Run this command?"):
+        print()
+        separator()
+        print()
+
+        console.print(
+            "[bold]Executing generated command:[/bold]"
+        )
+        print()
+        subprocess.run(command, shell=True)
